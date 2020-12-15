094e0b206b9c3577c95fe18ba40fcda9c90e34ba
ali <a_icam@yahoo.com>
2020-12-15 11:01:11 +0330

    Machings

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from os import listdir
 from os.path import isfile, join
+import numpy as np
+
 mypath = "sampleDoc"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 def extract_dictionaries(file_names):
-    files = []
+    files = {}
     for file in file_names:
         f = open(mypath + '/' + file)
-        files.append({'dicID': file, 'content': f.read().replace('\n', ' ').replace('\u200c', ' ').split(' ')})
+
+
+
+        files.update({file: f.read().
+                     replace('\n', ' ').
+                     replace('\u200c', ' ').
+                     replace('.', '').split(' ')})
         f.close()
     return files
 
 
+
+## Remove 3 most frequent term
+def matching_1(dics):
+    words = np.concatenate(list(dics.values()))
+    for i in range(3):
+        unique, pos = np.unique(words, return_inverse=True)
+        counts = np.bincount(pos)
+        maxpos = counts.argmax()
+        words = np.delete(words, np.argwhere(words == unique[maxpos]))
+        print(unique[maxpos])
+
 files_dic = extract_dictionaries(onlyfiles)
-print(files_dic[0])
+matching_1(files_dic)
