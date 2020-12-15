f30ba8683260852fa00d7eb32cd0d3eccdc17f26
ali <a_icam@yahoo.com>
2020-12-15 18:02:03 +0330

    search

--- search.py
+++ search.py
@@ -3,5 +3,10 @@ import numpy as np
 def search(ii, q):
     terms = q.split(' ')
     terms = [item for item in terms if item in ii.keys()]
-    print(np.where(ii.keys() == 'به'))
+
     terms_dict = {term: [item['docID'] for item in ii[term]] for term in terms}
+    doc_scores = {key: 0 for key in np.unique(np.hstack(list(terms_dict.values())))}
+    for term in terms:
+        for doc in list(set(terms_dict[term]).intersection(doc_scores.keys())):
+            doc_scores[doc] += 1
+    print(doc_scores)
