e24937f6b845346170e3e1ba8096108f97c87e43
ali <a_icam@yahoo.com>
2020-12-15 18:03:49 +0330

    search

--- search.py
+++ search.py
@@ -3,10 +3,9 @@ import numpy as np
 def search(ii, q):
     terms = q.split(' ')
     terms = [item for item in terms if item in ii.keys()]
-
     terms_dict = {term: [item['docID'] for item in ii[term]] for term in terms}
     doc_scores = {key: 0 for key in np.unique(np.hstack(list(terms_dict.values())))}
     for term in terms:
         for doc in list(set(terms_dict[term]).intersection(doc_scores.keys())):
             doc_scores[doc] += 1
-    print(doc_scores)
+    return({k: v for k, v in sorted(doc_scores.items(), key=lambda item: item[1], reverse=True)})
